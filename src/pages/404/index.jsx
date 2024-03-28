23db043e5bd0d533ebbc9eb0ab9fe2fcd07bcebc
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-03-28 09:14:19 +0100

    Add error page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,15 @@
+import './style.scss'
+import Img404 from '../../assets/404.png'
+import { Link } from 'react-router-dom';
+
 function Error404 () {
-    return (<p>Error</p>);
+    return (
+        <div className='error-page'>
+            <img src={Img404} alt='Erreur 404' className='error-img'/>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className='link-home'>Retourner sur la page d'accueil</Link>
+        </div>
+    );
 }
 
 export default Error404
