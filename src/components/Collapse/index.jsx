eaa165da7deae63102ffe80b459eecb2272cf078
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-03-28 15:21:02 +0100

    Layout Collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -10,13 +10,23 @@ function Collapse({title, content}) {
         setIsOpen(!isOpen);
     };
 
+    let contentList
+    if (Array.isArray(content)) {
+        contentList = content.map((item, index) => (
+            <li key={index}>{item}</li>
+        ));
+    } else {
+        contentList = <li>{content}</li>; // Afficher le contenu tel quel s'il n'est pas un tableau
+    }
+
+
     return (
         <div className='collapse'>
             <button className='collapse-toggle'>
                 <h2>{title}</h2>
                 {isOpen ? <img onClick={toggleCollapse} src={ArrowDown} alt='Flèche vers le bas'/> : <img onClick={toggleCollapse} src={ArrowUp} alt='Flèche vers le haut'/>}
             </button>
-            {isOpen && <p>{content}</p>}
+            {isOpen && <ul>{contentList}</ul>}
         </div>
     );
 }
