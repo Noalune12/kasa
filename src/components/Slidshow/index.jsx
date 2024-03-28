987cf53f79e2900b08a7ea7e4ec68c4e9265b05c
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-03-28 11:08:58 +0100

    Create slideshow component + add to annonce page

---
+++ src/components/Slidshow/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import ArrowUp from '../../assets/arrow-up.png'
+import './style.scss'
+
+function Slideshow ({images}) {
+    const [current, setCurrent] = useState(0);
+
+    const nextSlide = () => {
+        setCurrent(current === images.length -1 ? 0 : current + 1);
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? images.length - 1 : current - 1);
+    }
+
+    return (
+        <div className='slideshow'>
+            {images.length > 1 && (
+                <img src={ArrowUp} className='left-arrow' onClick={prevSlide} alt='Previous'/>
+            )}
+            {images.length > 1 && (
+                <img src={ArrowUp} className='right-arrow' onClick={nextSlide} alt='Next'/>
+            )}
+            {images.map((image, index) => (
+                <div key={index} className={index === current ? 'slide active' : 'slide'}>
+                    {index === current && <img key={index} src={image} alt={`${index}`}/>}
+                </div>                              
+            ))}
+        </div>
+    );
+}
+
+export default Slideshow
--- src/pages/Annonce/index.jsx
+++ src/pages/Annonce/index.jsx
@@ -1,5 +1,35 @@
+import Slideshow from "../../components/Slidshow";
+import { useFetch } from '../../utils/usefetch'
+import { useParams } from "react-router-dom";
+import React from "react";
+import './style.scss'
+
+
 function Annonce () {
-    return (<p>Logement</p>);
+    const annonces = useFetch(window.location.origin + '/logements.json')
+    const {annonceId} = useParams();
+    console.log(annonceId)
+
+    let thisAnnonce 
+    if(annonces.data) {
+        thisAnnonce = annonces.data.find(
+            (annonce) => annonce.id === annonceId
+        )
+    }
+    console.log(thisAnnonce)
+
+    if(annonces.isLoading) {
+        return <p>Chargement en cours...</p>;
+    }
+    if (annonces.error) {
+        return <p>Une erreur s'est produite</p>;
+    }
+
+    return (
+        <div className="annonce-page">
+            <Slideshow images={thisAnnonce.pictures} />
+        </div>
+       );
 }
 
 export default Annonce
