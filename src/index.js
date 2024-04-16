4a4b17084f3ae23b3f06136744cedb8b30d48777
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-04-16 15:42:13 +0200

    modif route error

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ root.render(
             <Route path="/" element={<Home />} />
             <Route path="/a-propos" element={<APropos />} />
             <Route path="/annonce/:annonceId" element={<Annonce />} />
-            <Route path="*" element={<Error404 />} />
+            <Route path="/*" element={<Error404 />} />
           </Routes>
         </div>
         <Footer />
